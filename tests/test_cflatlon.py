2949a40778917cee18d5f5a10cf5f2cf81ca387d
Leif Denby <leif@denby.eu>
2021-10-27 09:30:53 +0100

    start work on goes-16 crs support

--- tests/test_cflatlon.py
+++ tests/test_cflatlon.py
@@ -3,10 +3,13 @@ import datetime
 import pytz
 import rioxarray as rxr
 import regridcart as rc
+import xarray as xr
+import io
+import urllib
 from regridcart.coords import get_latlon_coords_using_crs
 
 
-def test_crop_and_resample_from_cf_coords():
+def test_raster_crs():
     t_now = datetime.datetime.now().replace(tzinfo=pytz.utc)
     t = t_now - datetime.timedelta(hours=2)
 
@@ -29,6 +32,36 @@ def test_crop_and_resample_from_cf_coords():
     )
 
     da = rxr.open_rasterio(fn_image)
+
+    da_cropped = rc.crop_field_to_domain(domain=target_domain, da=da, pad_pct=0.0)
+
+    dx = 10.0e3  # [m]
+    da_resampled = rc.resample(target_domain, da=da_cropped, dx=dx)
+    assert da_resampled is not None
+
+
+def test_goes16_crs():
+    url = "http://noaa-goes16.s3.amazonaws.com/ABI-L2-ACHAF/2021/001/15/OR_ABI-L2-ACHAF-M6_G16_s20210011520211_e20210011529519_c20210011530561.nc"
+
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as resp:
+        ds = xr.open_dataset(io.BytesIO(resp.read()))
+        da = ds.HT
+        da.coords[da.grid_mapping] = ds[da.grid_mapping]
+
+        for k, v in da.coords[da.grid_mapping].attrs.items():
+            try:
+                da.coords[da.grid_mapping].attrs[k] = float(v)
+            except ValueError:
+                pass
+
+    target_domain = rc.LocalCartesianDomain(
+        central_latitude=12.0,
+        central_longitude=-55.0,
+        l_meridional=100.0e3,
+        l_zonal=200.0e3,
+    )
 
     da_cropped = rc.crop_field_to_domain(domain=target_domain, da=da, pad_pct=0.0)
 
