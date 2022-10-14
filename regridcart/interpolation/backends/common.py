e02e93c559f1ff62101e3b9110c5dbd4bfa37fa8
leif <leif@denby.eu>
2022-10-14 11:07:10 +0000

    fix for xesmf >= 0.6.3

--- regridcart/interpolation/backends/common.py
+++ regridcart/interpolation/backends/common.py
@@ -21,4 +21,14 @@ def resample(
     if keep_attrs:
         da_resampled.attrs.update(da.attrs)
 
+    # as of xesmf >= 0.6.3 the lat/lon coords aren't copied any more
+    # I don't have time to understand
+    # https://github.com/pangeo-data/xESMF/pull/175, but I think that PR was
+    # actually meant to fix something that to me wasn't broken before. so I
+    # will copy in the new lat/lon here if they are missing
+
+    for c in ["lat", "lon"]:
+        if c not in da_resampled.coords:
+            da_resampled[c] = new_grid[c]
+
     return da_resampled
